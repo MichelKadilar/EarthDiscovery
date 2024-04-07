3a8db4bad059bf79b26ec1e76b98f84a1605f53f
Anas Chhilif <anas.chhilif@etu.unice.fr>
2024-04-07 17:55:22 +0200

    finishing up lab room

--- Assets/Planet 4/doorScript.cs
+++ Assets/Planet 4/doorScript.cs
@@ -4,5 +4,39 @@ using UnityEngine;
 
 public class doorScript : MonoBehaviour
 {
+    public float interactionDistance;
+    public GameObject intText;
+    public string doorOpenAnimName, doorCloseAnimName;
+
+    void Update()
+    {
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hit;
+
+        if(Physics.Raycast(ray, out hit, interactionDistance)){
+            if(hit.collider.gameObject.tag == "door"){
+            GameObject doorParent = hit.collider.transform.root.gameObject;
+                Animator doorAnim = doorParent.GetComponent<Animator>();
+                intText.SetActive(true);
+                if(Input.GetKeyDown(KeyCode.E))
+                {
+                    if(doorAnim.GetCurrentAnimatorStateInfo(0).IsName(doorOpenAnimName))
+                    {
+                        doorAnim.ResetTrigger("open");
+                        doorAnim.SetTrigger("close");
+                    }
+                    if(doorAnim.GetCurrentAnimatorStateInfo(0).IsName(doorCloseAnimName))
+                    {
+                        doorAnim.ResetTrigger("close");
+                        doorAnim.SetTrigger("open");
+                    }
+                }
+            }
+            else
+            {
+                intText.SetActive(false);
+            }
+        }
+    }
 
 }
